c449692fda4650b534a6e965852f9fc8fd4e3db1
Mastersam <abadasamuelosp@gmail.com>
2020-04-05 15:30:03 -0700

    widgets

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:tojuwa/screens/article_screen.dart';
+
+Widget MyAppBar() {
+  return AppBar(
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Toju",
+          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
+        ),
+        Text(
+          "Wa",
+          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.w600),
+        )
+      ],
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: Icon(Icons.share),
+        onPressed: null,
+      )
+    ],
+    backgroundColor: Colors.transparent,
+    elevation: 0.0,
+  );
+}
+
+class NewsTile extends StatelessWidget {
+  final String imgUrl, title, desc, content, posturl;
+
+  NewsTile({this.imgUrl, this.desc, this.title, this.content, @required this.posturl});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context) => ArticleView(
+              postUrl: posturl,
+            )
+        ));
+      },
+      child: Container(
+          margin: EdgeInsets.only(bottom: 24),
+          width: MediaQuery.of(context).size.width,
+          child: Container(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(bottomRight: Radius.circular(6),bottomLeft:  Radius.circular(6))
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(6),
+                      child: Image.network(
+                        imgUrl,
+                        height: 200,
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.cover,
+                      )),
+                  SizedBox(height: 12,),
+                  Text(
+                    title,
+                    maxLines: 2,
+                    style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    desc,
+                    maxLines: 2,
+                    style: TextStyle(color: Colors.black54, fontSize: 14),
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
